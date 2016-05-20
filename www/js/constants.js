b5051756853e6ede2757e181ffa30f3f05d1515c
Palotec <palotec@SuperMan.local>
2016-05-20 16:54:40 -0300

    first commit

---
+++ www/js/constants.js
@@ -0,0 +1,15 @@
+/**
+ * Created by palotec on 20/05/16.
+ */
+
+angular.module('starter')
+
+  .constant('AUTH_EVENTS', {
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+  })
+
+  .constant('USER_ROLES', {
+    admin: 'admin_role',
+    public: 'public_role'
+  });
